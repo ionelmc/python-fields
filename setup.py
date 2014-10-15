22a4aa9b47755f3d4b2db0fb9e3bd214461cd1c0
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-10-16 00:27:15 +0300

    Bump version: 1.0.3 → 2.0.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*names, **kwargs):
 
 setup(
     name="fields",
-    version="1.0.3",
+    version="2.0.0",
     license="BSD",
     description="A totally different take on container boilerplate.",
     long_description="%s\n%s" % (read("README.rst"), re.sub(":obj:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst"))),
--- src/fields/__init__.py
+++ src/fields/__init__.py
@@ -18,7 +18,7 @@ try:
 except ImportError:
     from .py2ordereddict import OrderedDict
 
-__version__ = "1.0.3"
+__version__ = "2.0.0"
 
 PY3 = sys.version_info[0] == 3
 MISSING = object()
