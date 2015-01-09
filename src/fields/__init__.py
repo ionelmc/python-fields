c621e5af0892bf69460cc2ab71d78502943351eb
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-01-09 20:04:08 +0200

    Spacing.

--- src/fields/__init__.py
+++ src/fields/__init__.py
@@ -30,6 +30,7 @@ def _with_metaclass(meta, *bases):
     class metaclass(meta):
         __call__ = type.__call__
         __init__ = type.__init__
+
         def __new__(cls, name, this_bases, d):
             if this_bases is None:
                 return type.__new__(cls, name, (), d)
