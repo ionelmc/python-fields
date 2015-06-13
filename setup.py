d1ec90e559ea18c7bc332cc9eb0fd95b2e27f4eb
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-06-13 19:00:25 +0300

    Bump version: 2.3.0 → 2.4.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*names, **kwargs):
 
 setup(
     name="fields",
-    version="2.3.0",
+    version="2.4.0",
     license="BSD",
     description="A totally different take on container boilerplate.",
     long_description="%s\n%s" % (read("README.rst"), re.sub(":obj:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst"))),
